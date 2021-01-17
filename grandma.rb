84f683d712b90631a6bfe1e37b72635b12605c30
Jose Soto <jsoto7087@gmail.com>
2021-01-16 21:22:36 -0500

    commit tree 3b54cb56674ed1e5fe22cca2294f8fd2e459ca38

--- grandma.rb
+++ grandma.rb
@@ -11,18 +11,32 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-speak_to_grandma = "I LOVE YOU GANDMA!"
-if speak_to_grandma == "I LOVE YOU TOO PUMPKIN!"
-    puts "I LOVE YOU GANDMA!"
- elsif speak_to_grandma == "i love you gandma!"
-    puts "HUH?! SPEAK UP, SONNY!"
-elsif speak_to_grandma == "Hi Nana, how are you?"
-    puts "HUH?! SPEAK UP, SONNY!"
-elsif speak_to_grandma == "Hi!"
-    puts "HUH?! SPEAK UP, SONNY!"
-elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
-    puts "NO, NOT SINCE 1938!"
-elsif speak_to_grandma == "WHAT?"
-    puts "NO, NOT SINCE 1938!"
-end
+#speak_to_grandma = "I LOVE YOU GANDMA!"
+#if speak_to_grandma == "I LOVE YOU TOO PUMPKIN!"
+ #   puts "I LOVE YOU GANDMA!"
+ #elsif speak_to_grandma == "i love you gandma!"
+  #  puts "HUH?! SPEAK UP, SONNY!"
+#elsif speak_to_grandma == "Hi Nana, how are you?"
+  #  puts "HUH?! SPEAK UP, SONNY!"
+#elsif speak_to_grandma == "Hi!"
+ #   puts "HUH?! SPEAK UP, SONNY!"
+#elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
+ #   puts "NO, NOT SINCE 1938!"
+#elsif speak_to_grandma == "WHAT?"
+ #   puts "NO, NOT SINCE 1938!"
+#end
+#end
+
+
+def speak_to_grandma(phrase)
+    if phrase == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    end
+
+ if  phrase.upcase != phrase
+    return "HUH?! SPEAK UP, SONNY!"
+ else 
+    return "NO, NOT SINCE 1938!"
+ end
+
 end
